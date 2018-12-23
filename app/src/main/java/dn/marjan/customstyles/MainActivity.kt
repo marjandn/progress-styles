8ba07795164d6b3ceed85bb4bedb46342a0b4850
Marjan DavoodiNejad <marjan.dn96@yahoo.com>
2018-12-23 17:13:17 +0330

    Initial commit

---
+++ app/src/main/java/dn/marjan/customstyles/MainActivity.kt
@@ -0,0 +1,12 @@
+package dn.marjan.customstyles
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_progress)
+    }
+}
